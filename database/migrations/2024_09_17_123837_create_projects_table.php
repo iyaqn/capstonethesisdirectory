a9ed59845a478f0a059579d4b99995c678b390c2
Keane Lee <keanejoshua2003@gmail.com>
2024-11-06 18:48:08 +0800

    fix

--- database/migrations/2024_09_17_123837_create_projects_table.php
+++ database/migrations/2024_09_17_123837_create_projects_table.php
@@ -18,10 +18,10 @@ return new class extends Migration
             $table->string('ipRegistration')->nullable();
             $table->string(column: 'specialization')->nullable();
             $table->string('title')->nullable();
-            // $table->string(column: 'author1')->nullable();  
-            // $table->string(column: 'author2')->nullable();
-            // $table->string(column: 'author3')->nullable();
-            // $table->string(column: 'author4')->nullable();
+            $table->string(column: 'author1')->nullable();  
+            $table->string(column: 'author2')->nullable();
+            $table->string(column: 'author3')->nullable();
+            $table->string(column: 'author4')->nullable();
             $table->string('technicalAdviser')->nullable();
             $table->string('yearPublished')->nullable();
             $table->string('fullDocument')->nullable();
